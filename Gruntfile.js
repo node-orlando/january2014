100355d263048827a2c61adf3ddcdcc7db28af47
Robb Schiller <robb.schiller@gmail.com>
2014-01-14 08:26:42 -0500

    sass gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,29 @@
 'use strict';
 
 module.exports = function(grunt) {
+	var base = __dirname+'/scss';
 
+	grunt.initConfig({
+		sass: {
+			options: {
+				style: 'expanded'
+			},
+			dev: {
+				files: {
+					'dist/styles.dev.css': base+'/main.scss'
+				},
+			},
+			prod: {
+				options: {
+					style: 'compressed'
+				},
+				files: {
+					'dist/styles.prod.css': base+'/main.scss'
+				},
+			},
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.registerTask('default', 'sass:dev');
 };
